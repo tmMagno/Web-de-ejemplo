69614aa2ff6c76013f715e48558067280e047166
TomiMagno <tomasmagno2019@gmail.com>
2021-05-30 16:00:26 -0300

    Pagina web basica

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const hamburguer = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
+
+//console.log(hamburguer,menu)
+
+hamburguer.addEventListener('click', ()=>{
+    menu.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread') && console.target != menu && e.target != hamburguer ){
+        menu.classList.toggle("spread")
+    }
+})
